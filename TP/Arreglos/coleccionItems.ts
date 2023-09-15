f856702b24121a2fd4fa2169cad8adbb3e7dea8c
hansaguillon <hansaguillon@gmail.com>
2023-09-14 21:26:14 -0300

    avances tp final coleciones

---
+++ TP/Arreglos/coleccionItems.ts
@@ -0,0 +1,38 @@
+import { itemslibreria } from "../Clases/itemlibre";
+
+
+class coleccionItems 
+{
+    private items:[];
+
+
+    public constructor()
+    {
+        this.items = [];
+    }
+
+
+
+    public agregarItem():void{
+        
+    }
+
+    public eliminarItem():void{
+
+    }
+
+    public buscarItemPorId():void{
+
+
+    }
+
+    public listarItems():void
+    {
+
+    }
+
+
+
+
+
+}
