3049e72a1c28a5e4b44c632fe281bcd02ce66900
hansaguillon <hansaguillon@gmail.com>
2023-11-02 22:01:56 -0300

    cosas nuevas

---
+++ Front/Fecth/index.js
@@ -0,0 +1,81 @@
+const productsList = document.getElementById("productlist");
+const btn = document.querySelectorAll("button");
+const randomProductContainer = document.getElementById("randomproduct");
+btn[0].addEventListener("click",getAll);
+btn[1].addEventListener("click",getOne);
+
+const URL = "https://fakestoreapi.com/products/";
+function getAll() {
+  fetch(URL, {
+    method: "get",
+  })
+    .then(res => res.json())
+    .then(products => render(products));
+}
+
+function getOne() {
+    const randomID = Math.floor(Math.random() * 20 + 1);
+    fetch(URL + randomID, {
+      method: "get",
+    })
+      .then(res => res.json())
+      .then(product => renderOne(product));
+  }
+
+  function render(arr) {
+//    clearProductsList();
+console.log(arr);
+    for (item of arr) {
+      const ListItem = document.createElement("li");
+      ListItem.textContent = `${item.title} - $${item.price}`;
+      productsList.appendChild(ListItem);
+    }
+  }
+  function renderOne(pepote) {
+    randomProductContainer.textContent = `${pepote.title} - $${pepote.price}`;
+  }
+  
+  function clearProductsList() {
+    productsList.querySelectorAll("*").forEach(child => child.remove());
+  }
+/*
+  const productsList = document.getElementById("productlist");
+const btn = document.querySelectorAll("button");
+const randomProductContainer = document.getElementById(
+  "randomproduct"
+);
+btn[0].addEventListener("click", getAll);
+btn[1].addEventListener("click", getOne);
+
+const URL = "https://fakestoreapi.com/products/";
+function getAll() {
+  fetch(URL, {
+    method: "get",
+  })
+    .then(res => res.json())
+    .then(products => render(products));
+}
+
+function getOne() {
+  const randomID = Math.floor(Math.random() * 20 + 1);
+  fetch(URL + randomID, {
+    method: "get",
+  })
+    .then(res => res.json())
+    .then(product => renderOne(product));
+}
+function render(arr) {
+  clearProductsList();
+  for (item of arr) {
+    const ListItem = document.createElement("li");
+    ListItem.textContent = `${item.title} - $${item.price}`;
+    productsList.appendChild(ListItem);
+  }
+}
+function renderOne(pepote) {
+  randomProductContainer.textContent = `${pepote.title} - $${pepote.price}`;
+}
+
+function clearProductsList() {
+  productsList.querySelectorAll("*").forEach(child => child.remove());
+}*/
